e3bb3e412ea47faae488a816fb66624de1b87d62
Milan <milansam360@gmail.com>
2020-06-14 13:28:38 +0530

    Applied MVC framework(for refrence) completed 70% work for deposit money

--- src/com/model_class/Customer.java
+++ src/com/model_class/Customer.java
@@ -1,5 +1,57 @@
 package com.model_class;
 
-public class Customer {
-
+public class Customer
+{
+	private int Customer_id,Customer_SSN_id,age;
+	private String Customer_name,address_1,address_2,city,state;
+	
+	public int getCustomer_id() {
+		return Customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		Customer_id = customer_id;
+	}
+	public int getCustomer_SSN_id() {
+		return Customer_SSN_id;
+	}
+	public void setCustomer_SSN_id(int customer_SSN_id) {
+		Customer_SSN_id = customer_SSN_id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCustomer_name() {
+		return Customer_name;
+	}
+	public void setCustomer_name(String customer_name) {
+		Customer_name = customer_name;
+	}
+	public String getAddress_1() {
+		return address_1;
+	}
+	public void setAddress_1(String address_1) {
+		this.address_1 = address_1;
+	}
+	public String getAddress_2() {
+		return address_2;
+	}
+	public void setAddress_2(String address_2) {
+		this.address_2 = address_2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
 }
